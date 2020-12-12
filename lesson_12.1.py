5648d3bb1a1647071649861ed2b305fe58ab6bdf
EgorBedokurov <73665129+EgorBedokurov@users.noreply.github.com>
2020-12-12 20:57:29 +0200

    Add files via upload

--- lesson_12.1.py
+++ lesson_12.1.py
@@ -1,45 +1,50 @@
-"""не закончено"""
 import uuid
+from datetime import date
 
 class Bank:
+    comission = 0.01
 
     """Банковский счет"""
-    def __init__(self, name, balance, transaction=[100], id=uuid.uuid4()):
-        self.name = name
-        self.balance = balance
-        self.transaction = transaction
-        self.id = id
+    def __init__(self, name_bank):
+        self.name_bank = name_bank #название банка
+        self._balance = 0
+        self._transactions = []
+        self.bank_account = uuid.uuid4() # id банка
+
+    # """учет операцй"""
+    @property
+    def current_balance(self):
+        deposit = 0
+        withdrawal = 0
+        for transaction in self._transactions:
+            if transaction[1] == 'deposit funds ':
+                deposit += transaction[0]
+            else:
+                withdrawal += transaction[0]
+        return deposit - withdrawal
 
     """депозит средств"""
-    def deposit(self):
-        dep = str(input('введите сумму на депозит - '))
-        return dep
-        print('ваш баланс ', str(self.balance - dep), 'на депозите - ' + str(dep))
-
+    def deposit(self, amount):
+        self._balance += amount*0.99
+        self._transactions.append((amount, 'deposit funds ', str(date.today())))
 
     """вывод средств"""
-    def withdrawal_of_funds(self):
-        w_t = input('введите сумму на депозит - ')
-        bal = self.balance - w_t
-        return w_t, bal
-        print('снято - ' + w_t + 'остаток на балансе - ' + bal)
+    def withdrawal_of_funds(self, amount): #если за лемитом все равно считает
+        if amount < self._balance:
+            self._balance -= amount*1.01
+        else:
+            print('not enough funds on the deposit ', str(self._balance))
+        self._transactions.append((amount, 'withdrawal funds ', str(date.today())))
 
 
-    """получить баланс"""
-    def get_balance(self):
-        g_b = self.balance
-        return g_b
-
-    """банковские комиссии"""
-    def commission(self):
-        com = self.transaction * 0.01
-        print('комиссия банка составила - ' + float(com))
-
-operation_1 = Bank('Omega Bank', 10000, 100, uuid.uuid4())
-print(operation_1.name)
-print(operation_1.balance)
-print(operation_1.id)
-print(operation_1.transaction)
-
-print(operation_1.deposit())
-operation_1.get_balance()
+operation = Bank('Omega Bank')
+print('Bank - ', operation.name_bank)
+print('Bank account - ', operation.bank_account)
+operation.deposit(3000)
+print('deposit of funds - ', operation._balance)
+operation.withdrawal_of_funds(2000)
+print('deposit of funds - ', operation._balance)
+operation.withdrawal_of_funds(700)
+print('Withdrawal of funds - ', operation._balance)
+print('archive of transactions - ', operation._transactions)
+print('current balance - ', operation._balance)
